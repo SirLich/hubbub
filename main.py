2e285acbd0c9377bff679419d7c15b7376556dec
SirLich <lrkoehler77@gmail.com>
2019-04-22 16:29:45 -0500

    Added next steps

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 VERSION = "v0.1"
 
+
+"""
+Next steps:
+    Figure out connection to the MongoDb
+        Using PyMongo (install using pip3)
+    Create MongoQuery from the yes_list, no_list
+    Sort query and print out
+    Populate the mongo database
+    test? Ha
+"""
+
 def main():
     yes_activities = []
     no_activities = []
@@ -25,8 +36,6 @@ def main():
 
     print("Calculating the perfect fit... ")
     print(no_activities)
-
-
 
 
 main()
