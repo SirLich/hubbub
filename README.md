972ea8141bca99e3fcc3cad9fd10e42fa59207e8
SirLich <lrkoehler77@gmail.com>
2019-04-24 16:16:49 -0500

    Created PyMongo connection

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # hubbub
-Fall 2019, Databases Final Project 
+Fall 2019, Databases Final Project
+
+Find tags: > db.activities.find({tags: "sport"})
+
+Not find tags: > db.activities.find({tags: {$ne: "sport"}})
+
+"""
+Next steps:
+    Figure out connection to the MongoDb
+        Using PyMongo (install using pip3)
+        pip3 install --user pymongo
+
+    Create MongoQuery from the yes_list, no_list
+    Sort query and print out
+    Populate the mongo database
+    test? Ha
+"""
--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
+from pymongo import MongoClient
+from pprint import pprint
+
 VERSION = "v0.1"
+
+def attemptMongoConnection():
+    cred_file = open("credentials.txt","r")
+    mongo_ip = cred_file.readline()
+    connection_string = "mongodb://" + mongo_ip.strip() + "/hubbub"
+    db = MongoClient(connection_string)
+    collection = db["activities"]
+    return collection
+
 
 
 """
 Next steps:
     Figure out connection to the MongoDb
         Using PyMongo (install using pip3)
+        pip3 install --user pymongo
+
     Create MongoQuery from the yes_list, no_list
     Sort query and print out
     Populate the mongo database
@@ -13,29 +27,28 @@ Next steps:
 """
 
 def main():
-    yes_activities = []
-    no_activities = []
+    yes_tags = []
+    no_tags = []
+
+    connection = attemptMongoConnection()
 
     print("Welcome to Hubbub!", VERSION)
-
-
-    print("Please enter some activities you are interested in!")
+    people = input("How many people are participating? ");
+    print("Please enter some tags you are interested in!")
     while(True):
         a = input("Enter: ")
         if(a == ""):
             break
-        yes_activities.append(a)
-
+        yes_tags.append(a)
 
     print("Please enter some activities you are NOT interested in!")
     while(True):
         a = input("Enter: ")
         if(a == ""):
             break
-        no_activities.append(a)
+        no_tags.append(a)
 
     print("Calculating the perfect fit... ")
-    print(no_activities)
-
+    print(no_tags)
 
 main()
