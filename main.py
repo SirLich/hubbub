b761562bc7dd95766268166ffb900900e062df92
SirLich <lrkoehler77@gmail.com>
2019-04-26 16:08:03 -0500

    Create activities hashmap and populate

--- main.py
+++ main.py
@@ -1,54 +1,86 @@
 #!/usr/bin/python3
 from pymongo import MongoClient
 from pprint import pprint
+from bson.objectid import ObjectId
 
 VERSION = "v0.1"
+
+activities_map = {}
 
 def attemptMongoConnection():
     cred_file = open("credentials.txt","r")
     mongo_ip = cred_file.readline()
-    connection_string = "mongodb://" + mongo_ip.strip() + "/hubbub"
-    db = MongoClient(connection_string)
-    collection = db["activities"]
-    return collection
+    connection_string = "mongodb://" + mongo_ip.strip()
+    connection = MongoClient(connection_string)
+    db = connection.hubbub
+    activities = db.activities
+    return activities
+
+
+db = attemptMongoConnection()
+
+def seed_activites_map():
+    for a in db.find():
+        activities_map[a["name"]] = 0
+
+def doQuery(tag):
+
+    fil = {
+    'tags': tag
+    }
+
+    for a in db.find(fil):
+        activities_map[a["name"]] = activities_map[a["name"]] + 1
+
+def print_activities():
+    for a in activities_map:
+        print(str(activities_map[a]), a)
+
+def temp_main():
+    seed_activites_map()
+    a = input("Enter a tag: ")
+    doQuery(a)
+    print_activities()
 
 
 
-"""
-Next steps:
-    Figure out connection to the MongoDb
-        Using PyMongo (install using pip3)
-        pip3 install --user pymongo
+temp_main()
 
-    Create MongoQuery from the yes_list, no_list
-    Sort query and print out
-    Populate the mongo database
-    test? Ha
-"""
 
-def main():
-    yes_tags = []
-    no_tags = []
+# def main():
+#     activities = attemptMongoConnection()
+#
+#     print("Welcome to Hubbub!", VERSION)
+#     people = input("How many people are participating? ");
+#from bson.objectid import ObjectId
 
-    connection = attemptMongoConnection()
+#     a = input("What is the time: [d]ay, [n]night >>> ")
+#     if(a == 'n'):
+#         doQuery(activities, "night")
+#     else if(a == 'd'):
+#         doQuery("day")
+#     else:
+#         print("Thats not valid!")
 
-    print("Welcome to Hubbub!", VERSION)
-    people = input("How many people are participating? ");
-    print("Please enter some tags you are interested in!")
-    while(True):
-        a = input("Enter: ")
-        if(a == ""):
-            break
-        yes_tags.append(a)
 
-    print("Please enter some activities you are NOT interested in!")
-    while(True):
-        a = input("Enter: ")
-        if(a == ""):
-            break
-        no_tags.append(a)
-
-    print("Calculating the perfect fit... ")
-    print(no_tags)
-
-main()
+    #
+    #
+    #
+    # print("Please enter some tags you are interested in!")
+    # while(True):
+    #     a = input("Enter: ")
+    #     if(a == ""):
+    #         break
+    #     yes_tags.append(a)
+    #
+    # print("Please enter some activities you are NOT interested in!")
+    # while(True):
+    #     a = input("Etemp_main()
+#nter: ")
+    #     if(a == ""):
+    # activities = attemptMongoConnection()
+    #         break
+    #     no_tags.append(a)
+    #
+    # print("Calculating the perfect fit... ")
+    # print(no_tags)
